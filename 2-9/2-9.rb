9202cf9f9ee77652b49471bf8a0a885127b96947
shigehitonaito <shigehito.7110.330@gmail.com>
2020-01-12 18:45:47 +0900

    [Finish]2-9

---
+++ 2-9/2-9.rb
@@ -0,0 +1,29 @@
+def hello_world
+  'Hello, World!'
+end
+
+puts hello_world
+
+
+# name ="shigehito"
+
+def greeting(name)
+  "Hello, #{name}!"
+
+end
+
+puts greeting('shigehito')
+
+
+
+def game
+return  'I win!'
+  'You lose!!'
+end
+
+puts game
+
+
+puts 1==2
+
+puts 8%3
